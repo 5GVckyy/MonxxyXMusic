46d50dab96eb30885d4d160b642ad99a61e5878e
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-03-11 02:16:45 +0700

    SteadyUB

--- XMusic/core/bot.py
+++ XMusic/core/bot.py
@@ -11,7 +11,7 @@ class XBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            "KayXMusic",
+            "MonxxyXMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
