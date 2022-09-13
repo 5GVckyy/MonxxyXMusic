b3f3d57408df1b47ccf2029c5ef53f77ee8e0537
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-09-14 06:34:35 +0700

    Update start.py

--- XMusic/utils/inline/start.py
+++ XMusic/utils/inline/start.py
@@ -2,17 +2,13 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton
 
-from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
+from config import SUPPORT_CHANNEL, SUPPORT_GROUP
 from XMusic import app
 
 
 def start_pannel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["S_B_1"],
-                url=f"https://t.me/{app.username}?start=help",
-            ),
             InlineKeyboardButton(
                 text=_["S_B_2"], callback_data="settings_helper"
             ),
@@ -53,11 +49,8 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_7"], callback_data="settings_back_helper"
-            ),
-            InlineKeyboardButton(
-                text=_["ST_B_6"], callback_data="LG"
-            ),
+                text=_["S_B_8"], callback_data="settings_back_helper"
+            )
         ]
     ]
     if SUPPORT_CHANNEL and SUPPORT_GROUP:
@@ -96,11 +89,4 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             )
         ]
     )
-    if GITHUB_REPO:
-        buttons.append(
-            [
-                InlineKeyboardButton(
-                    text=_["S_B_6"], url=f"{GITHUB_REPO}"),
-            ]
-        )
     return buttons
